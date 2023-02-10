37ec5ccef6b0e6e27c364bc1696ea9166a9736af
bokseongmin <bokseongmin@gmail.com>
2023-02-11 00:10:43 +0900

    day 12

--- Front/day12/server.js
+++ Front/day12/server.js
@@ -19,12 +19,12 @@ app.get("/", (req, res) => {
 
 // 목록
 var carList = [];
-for(var i=0; i<10; i++) {
-    carList.push({no:i, name:"car name "+i, price: (1+i)*1000, year:2008+i, company:"company"+i});
+for (var i = 0; i < 10; i++) {
+    carList.push({ no: i, name: "car name " + i, price: (1 + i) * 1000, year: 2008 + i, company: "company" + i });
 }
 let no = 10;
 // GET 요청 처리 - SELECT 기능
-app.get("/car", (req, res)=>{
+app.get("/car", (req, res) => {
     console.log("GET - /car");
     res.send(carList);
 });
@@ -32,9 +32,18 @@ app.get("/car", (req, res)=>{
 // 수정
 app.post("/car/modify", (req, res) => {
     console.log("POST - /car/modify " + req.body.no);
-    let modifyObj = req.body;
-    
 
+    let _obj = req.body;
+    let _no = parseInt(_obj.no);
+    let modifyObj = { no: _no, name: _obj.name, price: _obj.price, year: _obj.year, company: _obj.company };
+    let targetIndex;
+    for (let i = 0; i < carList.length; i++) {
+        if (carList[i].no == req.body.no) {
+            targetIndex = i;
+            break;
+        }
+    }
+    carList[targetIndex] = modifyObj;
     res.send(carList);
 });
 
@@ -48,7 +57,7 @@ app.post("/car/delete", (req, res) => {
 // POST 요청 처리 - INSERT 기능
 // post요청에서 파라미터를 받기위해서는 body-parser 미들웨어 필요.
 // 테스트는 Post Man으로 하면 된다.
-app.post("/car", (req, res)=>{
+app.post("/car", (req, res) => {
     console.log("POST - /car");
     let carObj = req.body;
     carObj.no = no++;
